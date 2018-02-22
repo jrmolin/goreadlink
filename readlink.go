6056e464153072b6ffa79cdb35099a6109aeef25
mo <jmolinatto@endgame.com>
2018-02-22 14:37:56 -0500

    first facsimile of readlink in go

---
+++ readlink.go
@@ -0,0 +1,28 @@
+// [_Command-line arguments_](http://en.wikipedia.org/wiki/Command-line_interface#Arguments)
+// are a common way to parameterize execution of programs.
+// For example, `go run hello.go` uses `run` and
+// `hello.go` arguments to the `go` program.
+
+package main
+
+import (
+	"os"
+	"fmt"
+	"path/filepath"
+)
+
+func main() {
+
+    // `os.Args` provides access to raw command-line
+    // arguments. Note that the first value in this slice
+    // is the path to the program, and `os.Args[1:]`
+    // holds the arguments to the program.
+	argsWithoutProg := os.Args[1:]
+
+	for index, value := range argsWithoutProg {
+		abs, _ := filepath.Abs(value)
+		fmt.Println(abs)
+	}
+
+}
+
